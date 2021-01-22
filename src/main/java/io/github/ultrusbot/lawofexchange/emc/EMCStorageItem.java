7d9f747e037eb6af55a5bd0026d75757dd9d9a02
Ultra <13109003+UltrusBot@users.noreply.github.com>
2021-01-22 02:51:52 -0500

    Updated EMCStorageItem comment & removed public statements

--- src/main/java/io/github/ultrusbot/lawofexchange/emc/EMCStorageItem.java
+++ src/main/java/io/github/ultrusbot/lawofexchange/emc/EMCStorageItem.java
@@ -4,12 +4,13 @@ import net.minecraft.item.ItemStack;
 
 /**
  * Used on items that store EMC in some way.
+ * For items that aren't acting as a storage, but do use EMC, getMaxEMC should return MAX_INT
  */
 public interface EMCStorageItem {
-    public int getEMC(ItemStack itemStack);
-    public int getMaxEMC();
-    public void setEMC(ItemStack itemStack, int emc);
-    public void addEMC(ItemStack itemStack, int emc);
-    public void removeEMC(ItemStack itemStack, int emc);
+    int getEMC(ItemStack itemStack);
+    int getMaxEMC();
+    void setEMC(ItemStack itemStack, int emc);
+    void addEMC(ItemStack itemStack, int emc);
+    void removeEMC(ItemStack itemStack, int emc);
 
 }
